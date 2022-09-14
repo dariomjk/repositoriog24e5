5d208d244adde33f2a270bd8f6030d9ce9be53bd
Dario Mojica <dario.mojica.mt@usa.edu.co>
2022-09-13 19:40:16 -0500

    Commit 002 Proyecto

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author dmojica
+ */
+public class Ejercicio2 {
+        public static void main(String[] args) {
+        System.out.println("Clase Git 02");
+    }
+    
+}
